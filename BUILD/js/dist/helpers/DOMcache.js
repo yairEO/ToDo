a107c6e45f4d4f0b894c43be44c17292e5ab1554
vsync <vsync.design@gmail.com>
2015-10-26 10:53:26 +0200

    -

---
+++ BUILD/js/dist/helpers/DOMcache.js
@@ -0,0 +1,9 @@
+/////////////////////////////////
+// Global cached DOM elements
+
+export default {
+    $HTML : $(document.documentElement),
+    $DOC  : $(document),
+    $WIN  : $(window),
+    $BODY : $(document.body)
+}
